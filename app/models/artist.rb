ebc79ab7459a7bd5e0b9f5d0d1c76538fc06604a
YOUR NAME <YOUR@EMAIL.com>
2016-06-14 02:01:22 +0530

    updated the seeds

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ActiveRecord::Base
+  belongs_to :artist
+  has_many :songs
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 Artists.create({ name: "Eminem"})
+Artists.create({ name: "The Beatles"})
+Artists.create({ name: "The Easybeats"})
+Artists.create({ name: "Eddy Grant"})
+
 
 Songs.create({ title: "Hi.. my name is!"})
-
+Songs.create({ title: "Here comes the sun"})
+Songs.create({ title: "Friday on my mind"})
+Songs.create({ title: "Electric avenue"})
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
