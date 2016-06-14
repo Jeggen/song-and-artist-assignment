1401dac4e87d99f9073c9a4af56bcffeb095e34c
YOUR NAME <YOUR@EMAIL.com>
2016-06-15 01:46:26 +0530

    update seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :songs
+    root 'artists#index'
+    resources :artists
   end
 
-  root 'artists#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +57,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
-eminem = Artists.create({ name: "Eminem"})
-the_beatles = Artists.create({ name: "The Beatles"})
-the_easybeats = Artists.create({ name: "The Easybeats"})
-eddy_grant = Artists.create({ name: "Eddy Grant"})
+eminem = Artist.create(name: "Eminem")
+the_beatles = Artist.create(name: "The Beatles")
+the_easybeats = Artist.create(name: "The Easybeats")
+eddy_grant = Artist.create(name: "Eddy Grant")
 
-Songs.create([
-{ title: "Hi.. my name is!" user:references artist_id: eminem }
-{ title: "Here comes the sun" user:references artist_id: the_beatles}
-{ title: "Friday on my mind" user:references artist_id: the_easybeats}
-{ title: "Electric avenue" user:references artist:id: eddy_grant}
-])
+
+Song.create([
+  {title: "Hi.. my name is!", user: eminem},
+  {title: "Here comes the sun", user: the_beatles},
+  {title: "Friday on my mind", user: the_easybeats},
+  {title: "Electric avenue", user: eddy_grant}
+  ])
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
